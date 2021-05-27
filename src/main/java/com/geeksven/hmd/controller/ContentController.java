a3a76e667b0c9949300f6c73cdfcd20ae5601b76
geeksven <sven.reinhardt06@googlemail.com>
2021-05-27 17:30:04 +0200

    cv

--- src/main/java/com/geeksven/hmd/controller/ContentController.java
+++ src/main/java/com/geeksven/hmd/controller/ContentController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
 
 @Controller
 public class ContentController {
 
+  @GetMapping("/")
+  public ModelAndView method() {
+    return new ModelAndView("redirect:/index");
+  }
+
   @GetMapping("/index")
   public String index(
-      @RequestParam(name = "name", required = false, defaultValue = "Herr Glawe")
+      @RequestParam(name = "name", required = false, defaultValue = "Stefan")
           String name, Model model) {
     model.addAttribute("events", Collections.EMPTY_LIST);
     model.addAttribute("name", name);
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -28,68 +28,13 @@ $(document).ready(() => {
     window.addEventListener("load", callbackFunc);
     window.addEventListener("scroll", callbackFunc);
 
-    var TxtType = function (el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function () {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) {
-            delta /= 2;
-        }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = false;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-        }
-
-        delta = 200;
-
-        setTimeout(function () {
-            that.tick();
-        }, delta);
-    };
-
 
     $("#hire-btn").click(() => {
-        $("#hire-content").empty();
+        let hire = $("#hire-target");
+        hire.empty();
+        hire.append("<h4 class=\"daimler-font-serif\">Fullstack Developer - Daimler</h4>");
+        hire.append("<p class=\"typewriter\">Creating awesome cloud applications!</p>");
 
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i = 0; i < elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-                new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
     });
 
 })
